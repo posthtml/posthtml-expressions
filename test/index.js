9a34a552075b805fb9c9e2f66d4c94ec82544a79
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-11-19 03:58:27 +0100

    test(expect): move expects to separate folder

--- test/index.js
+++ test/index.js
@@ -1,166 +1,198 @@
 const test = require('ava')
-const path = require('path')
+
+const join = require('path').join
+const readSync = require('fs').readFileSync
+
 const posthtml = require('posthtml')
-const exp = require('../lib')
-const {readFileSync} = require('fs')
-const fixtures = path.join(__dirname, 'fixtures')
+const expressions = require('../lib')
 
-test('basic', (t) => {
-  return matchExpected(t, 'basic', { locals: { test: 'wow' } })
+const fixture = (file) => {
+  return readSync(join(__dirname, 'fixtures', `${file}.html`), 'utf8')
+}
+
+const expect = (file) => {
+  return readSync(join(__dirname, 'expect', `${file}.html`), 'utf8')
+}
+
+function process (t, name, options, log = false) {
+  return posthtml([ expressions(options) ])
+    .process(fixture(name))
+    .then((result) => {
+      log && console.log(result.html)
+
+      return clean(result.html)
+    })
+    .then((html) => {
+      t.truthy(html === expect(name).trim())
+    })
+}
+
+function error (name, cb) {
+  return posthtml([ expressions() ])
+    .process(fixture(name))
+    .catch(cb)
+}
+
+function clean (html) {
+  return html.replace(/[^\S\r\n]+$/gm, '').trim()
+}
+
+test('Basic', (t) => {
+  return process(t, 'basic', { locals: { test: 'wow' } })
 })
 
-test('escaped html', (t) => {
-  return matchExpected(t, 'escape_html', { locals: { lt: '<', gt: '>' } })
+test('Escaped', (t) => {
+  return process(t, 'escape_html', { locals: { lt: '<', gt: '>' } })
 })
 
-test('unescaped', (t) => {
-  return matchExpected(t, 'unescaped', {
+test('Unescaped', (t) => {
+  return process(t, 'unescaped', {
     locals: { el: '<strong>wow</strong>' }
   })
 })
 
-test('custom delimiters', (t) => {
-  return matchExpected(t, 'custom_delimiters', {
+test('Delimiters', (t) => {
+  return process(t, 'custom_delimiters', {
     delimiters: ['{%', '%}'],
     unescapeDelimiters: ['{{%', '%}}'],
     locals: { test: 'wow' }
   })
 })
 
-test('expression spacing', (t) => {
-  return matchExpected(t, 'expression_spacing', { locals: { foo: 'X' } })
+test('Expressions - spacing', (t) => {
+  return process(t, 'expression_spacing', { locals: { foo: 'X' } })
 })
 
-test('expression error', (t) => {
-  return expectError('expression_error', (err) => {
+test('Expressions - error', (t) => {
+  return error('expression_error', (err) => {
     t.truthy(err.toString() === 'SyntaxError: Unexpected token ILLEGAL')
   })
 })
 
-test('conditional', (t) => {
-  return matchExpected(t, 'conditional', { locals: { foo: 'bar' } })
+test('Conditionals', (t) => {
+  return process(t, 'conditional', { locals: { foo: 'bar' } })
 })
 
-test('conditional - only "if" condition', (t) => {
-  return matchExpected(t, 'conditional_if', { locals: { foo: 'bar' } })
+test('Conditionals - only "if" condition', (t) => {
+  return process(t, 'conditional_if', { locals: { foo: 'bar' } })
 })
 
-test('conditional - no render', (t) => {
-  return matchExpected(t, 'conditional_norender', {})
+test('Conditionals - no render', (t) => {
+  return process(t, 'conditional_norender', {})
 })
 
-test('conditional - "if" tag missing condition', (t) => {
-  return expectError('conditional_if_error', (err) => {
+test('Conditionals - "if" tag missing condition', (t) => {
+  return error('conditional_if_error', (err) => {
     t.truthy(err.toString() === 'Error: the "if" tag must have a "condition" attribute')
   })
 })
 
-test('conditional - "elseif" tag missing condition', (t) => {
-  return expectError('conditional_elseif_error', (err) => {
+test('Conditionals - "elseif" tag missing condition', (t) => {
+  return error('conditional_elseif_error', (err) => {
     t.truthy(err.toString() === 'Error: the "elseif" tag must have a "condition" attribute')
   })
 })
 
-test('conditional - other tag in middle of statement', (t) => {
-  return matchExpected(t, 'conditional_tag_break', {})
+test('Conditionals - other tag in middle of statement', (t) => {
+  return process(t, 'conditional_tag_break', {})
 })
 
-test('conditional - nested conditionals', (t) => {
-  return matchExpected(t, 'conditional_nested', {})
+test('Conditionals - nested conditionals', (t) => {
+  return process(t, 'conditional_nested', {})
 })
 
 test('conditional - expression error', (t) => {
-  return expectError('conditional_expression_error', (err) => {
+  return error('conditional_expression_error', (err) => {
     t.truthy(err.toString() === 'SyntaxError: Unexpected token ILLEGAL')
   })
 })
 
-test('conditional - custom tags', (t) => {
-  return matchExpected(t, 'conditional_customtags', {
+test('Conditionals - custom tags', (t) => {
+  return process(t, 'conditional_customtags', {
     conditionalTags: ['zif', 'zelseif', 'zelse'],
     locals: { foo: 'bar' }
   })
 })
 
-test('conditional - expression in else/elseif', (t) => {
-  return matchExpected(t, 'conditional_expression', {
+test('Conditionals - expression in else/elseif', (t) => {
+  return process(t, 'conditional_expression', {
     locals: { foo: 'bar' }
   })
 })
 
-test('loop', (t) => {
-  return matchExpected(t, 'loop', { locals: { items: [1, 2, 3] } })
+test('Loops', (t) => {
+  return process(t, 'loop', { locals: { items: [1, 2, 3] } })
 })
 
-test('loop object', (t) => {
-  return matchExpected(t, 'loop_object', {
+test('Loops - {Object}', (t) => {
+  return process(t, 'loop_object', {
     locals: { items: { a: 'b', c: 'd' } }
   })
 })
 
-test('loop with other locals included', (t) => {
-  return matchExpected(t, 'loop_locals', {
-    locals: { items: [1, 2, 3], foo: 'bar' }
-  })
-})
-
-test('loop with conflicting locals', (t) => {
-  return matchExpected(t, 'loop_conflict', {
-    locals: { items: [1, 2, 3], item: 'bar' }
-  })
-})
-
-test('nested loops', (t) => {
-  return matchExpected(t, 'loop_nested', {
+test('Loops - nested', (t) => {
+  return process(t, 'loop_nested', {
     locals: { items: { c1: [1, 2, 3], c2: [4, 5, 6] } }
   })
 })
 
-test('loop with included conditional', (t) => {
-  return matchExpected(t, 'loop_conditional', {
+test('Loops - locals included', (t) => {
+  return process(t, 'loop_locals', {
+    locals: { items: [1, 2, 3], foo: 'bar' }
+  })
+})
+
+test('Loops - conditional included', (t) => {
+  return process(t, 'loop_conditional', {
     locals: { items: [1, 2, 3] }
   })
 })
 
-test('loop - custom tag', (t) => {
-  return matchExpected(t, 'loop_customtag', {
+test('Loops - conflicting locals', (t) => {
+  return process(t, 'loop_conflict', {
+    locals: { items: [1, 2, 3], item: 'bar' }
+  })
+})
+
+test('Loops - custom tag', (t) => {
+  return process(t, 'loop_customtag', {
     loopTags: ['zeach'],
     locals: { items: [1, 2, 3] }
   })
 })
 
-test('loop - no loop attribute', (t) => {
-  return expectError('loop_no_attr', (err) => {
+test('Loops - no loop attribute', (t) => {
+  return error('loop_no_attr', (err) => {
     t.truthy(err.toString() === 'Error: the "elseif" tag must have a "loop" attribute')
   })
 })
 
-test('loop - no array or object passed', (t) => {
-  return expectError('loop_no_collection', (err) => {
+test('Loops - no array or object passed', (t) => {
+  return error('loop_no_collection', (err) => {
     t.truthy(err.toString() === 'Error: You must provide an array or object to loop through')
   })
 })
 
-test('loop - no loop arguments', (t) => {
-  return expectError('loop_no_args', (err) => {
+test('Loops - no loop arguments', (t) => {
+  return error('loop_no_args', (err) => {
     t.truthy(err.toString() === 'Error: You must provide at least one loop argument')
   })
 })
 
-test('loop - no "in" keyword', (t) => {
-  return expectError('loop_no_in', (err) => {
+test('Loops - no "in" keyword', (t) => {
+  return error('loop_no_in', (err) => {
     t.truthy(err.toString() === "Error: Loop statement lacking 'in' keyword")
   })
 })
 
-test('loop - expression error', (t) => {
-  return expectError('loop_expression_error', (err) => {
+test('Loops - expression error', (t) => {
+  return error('loop_expression_error', (err) => {
     t.truthy(err.toString() === 'SyntaxError: Unexpected token ILLEGAL')
   })
 })
 
-test('scope', (t) => {
-  return matchExpected(t, 'scope', {
+test('Scopes', (t) => {
+  return process(t, 'scope', {
     locals: {
       author: { name: 'John', age: 26 },
       name: 'Scope',
@@ -169,8 +201,8 @@ test('scope', (t) => {
   })
 })
 
-test('nested scope', (t) => {
-  return matchExpected(t, 'scope_nested', {
+test('Scopes - nested', (t) => {
+  return process(t, 'scope_nested', {
     locals: {
       key: 'global',
       scope: {
@@ -185,29 +217,3 @@ test('nested scope', (t) => {
     }
   })
 })
-
-//
-// Utility
-//
-
-function matchExpected (t, name, config, log = false) {
-  const html = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
-  const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
-
-  return posthtml([exp(config)])
-    .process(html)
-    .then((res) => { log && console.log(res.html); return cleanHtml(res.html) })
-    .then((html) => { t.truthy(html === expected.trim()) })
-}
-
-function expectError (name, cb) {
-  const html = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
-
-  return posthtml([exp()])
-    .process(html)
-    .catch(cb)
-}
-
-function cleanHtml (html) {
-  return html.replace(/[^\S\r\n]+$/gm, '').trim()
-}
