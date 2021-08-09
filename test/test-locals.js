d429c4aaeaf9c1627df1bf1e08c93198a226f0de
Ivan Demidov <Scrum@list.ru>
2021-08-09 17:35:18 +0300

    test: locals from script tag, issue #117

---
+++ test/test-locals.js
@@ -0,0 +1,36 @@
+const test = require('ava')
+
+const join = require('path').join
+const readSync = require('fs').readFileSync
+
+const posthtml = require('posthtml')
+const expressions = require('../lib')
+
+const fixture = (file) => {
+  return readSync(join(__dirname, 'fixtures', `${file}.html`), 'utf8')
+}
+
+const expect = (file) => {
+  return readSync(join(__dirname, 'expect', `${file}.html`), 'utf8')
+}
+
+function process (t, name, options, log = false, plugins = [expressions(options)], processOptions = {}) {
+  return posthtml(plugins)
+    .process(fixture(name), processOptions)
+    .then((result) => {
+      log && console.log(result.html)
+
+      return clean(result.html)
+    })
+    .then((html) => {
+      t.is(html, expect(name).trim())
+    })
+}
+
+function clean (html) {
+  return html.replace(/[^\S\r\n]+$/gm, '').trim()
+}
+
+test('Basic', (t) => {
+  return process(t, 'script-locals')
+})
